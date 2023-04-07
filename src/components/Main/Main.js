1da0c3b91adf692fa4758b39bf8e1985e0e2909e
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-07 18:47:21 -0400

    moved the state to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,19 @@
 import "./Main.scss";
 import Hero from "../Hero/Hero";
+import videos from '../../data/videos.json';
+import videoDetails from '../../data/video-details.json'
+import { useState } from 'react';
 
 function Main(){
+    const [selectedVideo, setSelectedVideo] = useState (videoDetails[0])
+
+    const videoClick = (videoId) => {
+      const foundVideo = videoDetails.find(video => video.id === videoId)
+      setSelectedVideo(videoClick);
+    }
+  
+    const filteredVideos = videos.filter(video => video.id !== selectedVideo.id)
+
     return(
         <main className="main">
             <Hero />
