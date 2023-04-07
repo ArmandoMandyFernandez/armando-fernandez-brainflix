e2efeaabdfd47f862fcdb6a8b426a9a9d0a60093
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-06 23:42:13 -0400

    header completed ready to merge to sprint 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,50 @@
-import './Header.scss';
-import logo from '../../assets/images/Logo/BrainFlix-logo.svg'
-import avatar from '../../assets/images/Mohan-muruge.jpg'
-import upload from '../../assets/images/Icons/upload.svg'
+import "./Header.scss";
+import logo from "../../assets/images/Logo/BrainFlix-logo.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import upload from "../../assets/images/Icons/upload.svg";
+import search from "../../assets/images/Icons/search.svg";
 
-function Header(){
+function Header() {
     return (
-    <header className="navBar">
-        <div className="navBar__content">
-            <div className='navBar__logo-container'>
-                <img className="navBar__logo" src={logo} />
+        <header className="navBar">
+            <div className="navBar__content">
+                <div className="navBar__logo-container">
+                    <img className="navBar__logo" src={logo} />
+                </div>
+                <div className="navBar__functions">
+                    <div className="navBar__feature-container">
+                        <div className="navBar__functions-search-container">
+                            <button className="navBar__functions-search-button">
+                                <img
+                                    src={search}
+                                    alt="Search icon"
+                                    className="navBar__functions-search-icon"
+                                />
+                            </button>
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className="navBar__functions-search"
+                            />
+                        </div>
+                        <img
+                            className="navBar__functions-avatar-1"
+                            type="submit"
+                            src={avatar}
+                        />
+                    </div>
+                    <button className="navBar__functions-button">
+                        <img
+                            className="navBar__functions-upload-image"
+                            src={upload}
+                        />
+                        <span className="navBar__functions-upload">UPLOAD</span>
+                    </button>
+                    <img className="navBar__functions-avatar-2" src={avatar} />
+                </div>
             </div>
-            <div className="navBar__functions">
-                <input type="text" placeholder="Search" className="navBar__functions-search"></input>
-                <img className="navBar__functions-avatar-1" type='submit' src={avatar} />
-            <button className="navBar__functions-button">
-                <img className='navBar__functions-upload-image' src={upload} />
-                <span className='navBar__functions-upload'>UPLOAD</span>
-            </button>
-            <img className="navBar__functions-avatar-2" type='submit' src={avatar} />
-            </div>
-        </div>
-    </header>
-    )
+        </header>
+    );
 }
 
-export default Header
+export default Header;
