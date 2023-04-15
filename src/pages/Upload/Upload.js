f0e8c671c464508634b31687e125253a5c89c877
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-15 16:40:50 -0400

    styling and adding of buttons on the page

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,4 +1,6 @@
-import thumbnail from '../../assets/images/Upload-video-preview.jpg'
+import thumbnail from "../../assets/images/Upload-video-preview.jpg";
+import publish from "../../assets/images/Icons/publish.svg";
+import "./Upload.scss";
 
 const Upload = () => {
     return (
@@ -6,20 +8,51 @@ const Upload = () => {
             <h1 className="upload__title">Upload Video</h1>
             <div className="upload__container">
                 <div className="upload__thumbnail-container">
-					<p className='upload__thumbnail-label'>Video Thumbnail</p> 
-					<img src={thumbnail} alt="first person view of riding a fixie bike"  className='upload__thumbnail-image'/>
-				</div>
-                <div>
-                    <form action="submit">
-                        <label htmlFor=""> TITLE YOUR VIDEO
-                            <input type="text" placeholder='Add a title to your video'/>
+                    <p className="upload__thumbnail-label">Video Thumbnail</p>
+                    <div className="upload__thumbnail-image-container">
+                        <img
+                            src={thumbnail}
+                            alt="first person view of riding a fixie bike"
+                            className="upload__thumbnail-image"
+                        />
+                    </div>
+                </div>
+                <div className="upload__form-container">
+                    <form action="submit" className="upload__form">
+                        <label htmlFor="" className="upload__form-label">
+                            {" "}
+                            TITLE YOUR VIDEO
+                            <input
+                                type="text"
+                                placeholder="Add a title to your video"
+                                className="upload__form-input"
+                            />
                         </label>
-                        <label htmlFor="">ADD A VIDEO DESCRIPTION
-                            <input type="text" placeholder='Add a description to your video'/>
+                        <label htmlFor="" className="upload__form-label">
+                            ADD A VIDEO DESCRIPTION
+                            <input
+                                type="text"
+                                placeholder="Add a description to your video"
+                                className="upload__form-input"
+                            />
                         </label>
                     </form>
                 </div>
-				<div>buttons</div>
+                <div className="upload__buttons-container">
+                    <button className="upload__button">
+                        <img
+                            src={publish}
+                            alt=""
+                            className="upload__button-image"
+                        />
+                        <span className="upload__button-description">
+                            PUBLISH
+                        </span>
+                    </button>
+                    <button className="upload__button-cancel">
+                        <span className="upload__button-description">CANCEL</span>
+                    </button>
+                </div>
             </div>
         </section>
     );
