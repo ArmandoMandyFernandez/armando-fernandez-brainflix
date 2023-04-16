d827302c580c798ad5fb04c0ab6f25d993f58748
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-16 14:16:14 -0400

    making adjustments to the 404 page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,12 @@
+import fourOhFour from '../../assets/images/—Pngtree—404 error page_1362453.png';
+
 const NotFound = () => {
-    return <>
-    <h1>404: Page Not Found</h1>
-    </>;
+    return(
+    <section>
+        <img src={fourOhFour} alt="404 page logo" />
+        <h1>Page Not Found</h1>
+    </section>
+    )
 };
 
 export default NotFound;
